9c2bf0c138f19342a3d42ee934eacc1fa9ab6f40
va-ovsyanikov <va.ovsyanikov@gmail.com>
2023-02-09 15:58:48 +0300

    add prettier

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ import VueCompositionAPI from '@vue/composition-api'
 Vue.config.productionTip = false
 Vue.use(VueCompositionAPI)
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App),
 }).$mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,12 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 Vue.use(VueRouter)
 
-const routes: Array<RouteConfig> = [
-
-]
+const routes: Array<RouteConfig> = []
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes,
 })
 
 export default router
